b78e25e29b908107ff9a9fb9607997392184a351
SruthiMadhuri <2115022@cognizant.com>
2022-11-02 21:50:27 +0530

    programs

---
+++ sample.kt
@@ -0,0 +1,17 @@
+// fun main(){
+//     val line=readln()
+//     println(line)
+// }
+// fun main(){
+//     val one = "1"
+//     val two = "2"
+//     val twelve = two + one 
+//     println(one)      // 1, no changes
+//     println(two)      // 2, no changes
+//     println(twelve)
+// }
+fun main(){
+    val a = 14
+    val b = 11
+    println("$a+$b")
+}
--- swap.kt
+++ swap.kt
@@ -1,20 +1,33 @@
-fun main() {
+// fun main() {
 
-    var first = 1.20f
-    var second = 2.45f
+//     var first = 1.20f
+//     var second = 2.45f
 
-    println("--Before swap--")
-    println("First number = $first")
-    println("Second number = $second")
+//     println("--Before swap--")
+//     println("First number = $first")
+//     println("Second number = $second")
 
     
-    val temporary = first
+//     val temporary = first
 
-    first = second
+//     first = second
 
-    second = temporary
+//     second = temporary
 
-    println("--After swap--")
-    println("First number = $first")
-    println("Second number = $second")
+//     println("--After swap--")
+//     println("First number = $first")
+//     println("Second number = $second")
+// }
+fun main(){
+    var fir="Kotlin"
+    var sec="Language"
+    val temporary = fir
+
+    fir = sec
+
+    sec = temporary
+
+    
+    println(fir)
+    println(sec)
 }
